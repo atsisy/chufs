b6facaee494ff35b8c37d4f836f1b25b71e69aa3
atsisy <at.sisy@gmail.com>
2017-04-15 16:07:27 +0900

    CommandLineWizクラスの空白処理のバグを修正
    再帰処理に移行する際、文字を前に進めていなかった

--- src/util/cmdline_wiz.cc
+++ src/util/cmdline_wiz.cc
@@ -12,23 +12,25 @@
  *char target
  *検索する文字
  *返り値
- *
+ *処理後の文字列のスマートポインタ
  *=======================================================================================
  */
-static std::unique_ptr<std::string> str_shrink(std::unique_ptr<std::string> str, short index, char target){
-      bool flag = false;
+static std::unique_ptr<std::string> str_shrink(std::unique_ptr<std::string> str, short index, char target, bool flag = false){
 
       for(unsigned short i = index; i < str->length(); ++i){
+            //ターゲットの文字か？
             if(str->data()[i] == target){
-                  if(!flag){
+                  if(!flag) {
+                        //まだ一回目
                         flag = true;
-                  }
-                  else{
-                        str_shrink(std::move(str), i, target);
+                  } else {
+                        //二回連続で出てきたら一文字進めて再帰処理
+                        str = str_shrink(std::move(str), i + 1, target, flag);
+                        //再帰から戻ってきたら消す
                         str->erase(str->begin() + i);
                   }
-            }
-            else{
+            } else {
+                  //基本的にはフラグをfalseに設定
                   flag = false;
             }
       }
@@ -38,19 +40,45 @@ static std::unique_ptr<std::string> str_shrink(std::unique_ptr<std::string> str,
 /*
  *=======================================================================================
  *コンストラクタ
+ *引数なし
+ *メンバオブジェクトを生成するだけ
  *=======================================================================================
  */
 CommandLineWiz::CommandLineWiz()
-      : origin(new std::string), word_vector(new std::vector<std::string>) {}
-
-CommandLineWiz::CommandLineWiz(const char *command_line)
-      : origin(new std::string(command_line)), word_vector(new std::vector<std::string>)
+      : origin(new std::string), word_vector(new std::vector<std::string>)
 {
+      /*
+       *メンバオブジェクトを生成するだけ
+       */
+}
+
+/*
+ *=======================================================================================
+ *コンストラクタ
+ *引数
+ *const char *command_line
+ *処理するコマンド
+ *コマンドを分割して、word_vectorにぶち込む
+ *=======================================================================================
+ */
+CommandLineWiz::CommandLineWiz(const char *command_line)
+      : word_vector(new std::vector<std::string>)
+{
+      //文字列の先頭がスペースになるのを防ぐ
+      while(*command_line == ' ') ++command_line;
+
+      //先頭のスペース除去後、originを設定
+      origin.reset(new std::string(command_line));
+
+      //StringStreamクラスを使って処理する
       std::stringstream string_stream(command_line);
+      //一時的に使う文字列変数
       std::string buffer;
 
+      //処理する過程で邪魔になる連続スペースを圧縮
       origin = str_shrink(std::move(origin), 0, ' ');
 
+      //ここで分割して、word_vectorにぶち込む
       while(std::getline(string_stream, buffer, ' ')) {
             word_vector->push_back(buffer);
       }
@@ -78,6 +106,7 @@ CommandLineWiz::~CommandLineWiz(){
  *=======================================================================================
  */
 std::string CommandLineWiz::At(int index){
+      //vectorの値を見て返すだけ
       return word_vector->at(index);
 }
 
