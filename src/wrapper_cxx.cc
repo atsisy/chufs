a89709bb1d0990861195169c75cdab4124d72dc5
atsisy <at.sisy@gmail.com>
2017-04-16 22:56:34 +0900

    C++のラッパ関数に返り値を持たせた
    一応すべて成功扱い（1を返す）

--- src/wrapper_cxx.cc
+++ src/wrapper_cxx.cc
@@ -3,10 +3,24 @@
 
 extern CHVFS_MANAGER *manager;
 
+/*
+ *=======================================================================================
+ *read_ata_sector_cxx_wrapper関数
+ *read_ata_sector関数（C側）のC++実装
+ *=======================================================================================
+ */
 i32_t read_ata_sector_cxx_wrapper(u32_t lba, void *buffer, i32_t count){
       manager->Read(lba, buffer, count);
+      return 1;
 }
 
+/*
+ *=======================================================================================
+ *write_ata_sector_cxx_wrapper関数
+ *write_ata_sector関数（C側）のC++実装
+ *=======================================================================================
+ */
 i32_t write_ata_sector_cxx_wrapper(u32_t lba, void *buffer, i32_t count){
       manager->Write(lba, buffer, count);
+      return 1;
 }
